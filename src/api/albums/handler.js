f64d444c72d6e0a53df25e9835fbb89b7bfb2d2b
nrhdyt3012 <dayats354313@gmail.com>
2026-01-03 12:08:59 +0700

    done king

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -30,11 +30,15 @@ class AlbumsHandler {
   async getAlbumByIdHandler(request, h) {
     const { id } = request.params;
     const album = await this._service.getAlbumById(id);
+    const songs = await this._service.getSongsByAlbumId(id);
 
     return {
       status: "success",
       data: {
-        album,
+        album: {
+          ...album,
+          songs,
+        },
       },
     };
   }
